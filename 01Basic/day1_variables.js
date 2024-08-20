b703090219f43519a9d00b5f2f5874d92e1b78bb
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-08-20 10:11:57 +0000

    DataTypes and variables

---
+++ 01Basic/day1_variables.js
@@ -0,0 +1,28 @@
+"use strict";
+// const accountId = 1234;
+// let accountEmail = "ztejd@example.com";
+// var accountCity = "Islamabad";
+// // accountCountry = "Pakistan";
+// console.log(accountId);
+// console.log(accountEmail);
+
+// Data Types 
+
+let age = 16;
+let isLoggedIn = false;
+let state = null;
+// number 
+// string
+// bigInt
+// boolean
+// undefined
+// null
+// symbol
+// object
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof undefined);
+
+
+
