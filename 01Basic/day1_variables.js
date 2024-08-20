73b61bbcf3ec474529146565aca20c3f04a67272
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-08-20 10:40:23 +0000

    operations

--- 01Basic/day1_variables.js
+++ 01Basic/day1_variables.js
@@ -8,9 +8,9 @@
 
 // Data Types 
 
-let age = 16;
-let isLoggedIn = false;
-let state = null;
+// let age = 16;
+// let isLoggedIn = false;
+// let state = null;
 // number 
 // string
 // bigInt
@@ -19,10 +19,26 @@ let state = null;
 // null
 // symbol
 // object
-console.log(typeof age);
-console.log(typeof isLoggedIn);
-console.log(typeof state);
-console.log(typeof undefined);
+// console.log(typeof age);
+// console.log(typeof isLoggedIn);
+// console.log(typeof state);
+// console.log(typeof undefined);
 
 
+
+// Data Type Conversions
+
+let score = true;
+
+console.log(typeof score);
+let valeInNumber = Number(score);
+console.log(typeof valeInNumber);
+console.log(valeInNumber);
+
+
+// Number convert easily, 
+// if we add mix of number and string it convert it to NaN,
+// if convert string it convert NaN
+// if we convert boolean it convert it to 1 or 0
+// if we convert undefined it convert it to NaN,
 
---
+++ 01Basic/operations.js
@@ -0,0 +1,35 @@
+"use strict";
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+// // +, -, *, /, %, **
+// console.log(12 + 2);
+// console.log(12 - 2);
+// console.log(12 * 2);
+// console.log(12 / 2);
+// console.log(12 % 2);
+// console.log(12 ** 2);
+// let str1 = "Hello";
+// let str2 = " Qadeer";
+// let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
