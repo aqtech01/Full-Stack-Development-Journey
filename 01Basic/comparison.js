61f375a74f5fed1810ed536a230cf1317dbf1fa7
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-08-21 12:19:30 +0000

    Data Type Summary Complete

---
+++ 01Basic/comparison.js
@@ -0,0 +1,58 @@
+"use strict";
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 === 1);
+
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null === 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// console.log(undefined === 0);
+// console.log(undefined == null);
+// console.log(undefined === null);
+
+
+// data Types Summary
+// 1. Primitive Data Types
+// Number
+// String
+// Boolean
+// Undefined
+// Null 
+// Symbol
+// BigInt
+// 2. Non-Primitive Data Types
+// Object
+// const obj = {
+//   MarvelHeros: ["Iron Man", "Captain America", "Thor", "Hulk", "Spider Man"],
+//   DcHeros: ["Superman", "Batman", "Wonder Woman", "Flash", "Green Lantern"]
+// }
+// // Array
+// const heros = ["Iron Man", "Captain America", "Thor", "Hulk", "Spider Man"]
+// // Function
+// const myFunc = () => {
+//   console.log("Hello World");
+// }
+// // Date
+// const dateType = new Date();
+// console.log(dateType);
+
+
+
+
+// const score = 100;
+// const scoreValue = 100.3;
+// const isLoggedIn = true;
+// const outSideTemp = null;
+// let userEmail;
+// const id = Symbol("123");
+// const bigNumber = BigInt(123);
+// console.log(score)
+
+
+
