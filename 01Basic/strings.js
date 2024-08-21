e0604318012a099d48ecb27f304b6d22a5ef8563
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-08-21 12:58:29 +0000

    Strings Complete

---
+++ 01Basic/strings.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const name = "Abdul Qadeer";
+const age = 25;
+// console.log(name + " and your age is " + age);
+// console.log(`My name is ${name.toUpperCase()} and my age is ${age}`);
+// console.log(name.length);
+// console.log(name.toLowerCase());
+// console.log(name.split(" "));
+// console.log(name.indexOf("Q"));
+// console.log(name.charAt(7));
+// console.log(name.substring(0, 6));
+// console.log(name.slice(0, 6));
+// console.log(name.endsWith("er"));
+// console.log(name.startsWith("Abdul"));
+// console.log(name.includes("r"));
