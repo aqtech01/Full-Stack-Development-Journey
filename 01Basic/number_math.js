3e1bb43babed87d50beef83d7f7270198e78b47d
aqtech <abdulqadeer0120@gmail.com>
2024-08-24 14:46:41 +0100

    numbers and math Function complete

--- 01Basic/number_math.js
+++ 01Basic/number_math.js
@@ -1,10 +1,23 @@
 "use strict";
 const score = new Number(10);
-console.log(score);
-console.log(score.toString);
-console.log(score.valueOf());
-console.log(score.toFixed(2));
-console.log(score.toPrecision(2));
-console.log(score.toExponential(2));
+// console.log(score);
+// console.log(score.toString);
+// console.log(score.valueOf());
+// console.log(score.toFixed(2));
+// console.log(score.toPrecision(2));
+// console.log(score.toExponential(2));
 
-console.log(score.toLocaleString());
+// console.log(score.toLocaleString());
+
+// Maths
+// console.log(Math.random().toFixed(1) * 10 + 1);
+// console.log(Math.PI.toFixed(2));
+// const heros = ["Capitan America", "Iron Man", "Black Widow"];
+// console.log(Math.random());
+// console.log(Math.abs(-5));
+// console.log(Math.sqrt(5));
+// console.log(Math.round(4.555));
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
