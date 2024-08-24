1dc91c42ed0b0b4c82907f4225329c7d425bb664
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-08-24 11:39:50 +0000

    practise some string methods

---
+++ 01Basic/number_math.js
@@ -0,0 +1,10 @@
+"use strict";
+const score = new Number(10);
+console.log(score);
+console.log(score.toString);
+console.log(score.valueOf());
+console.log(score.toFixed(2));
+console.log(score.toPrecision(2));
+console.log(score.toExponential(2));
+
+console.log(score.toLocaleString());
---
+++ practise/strings.js
@@ -0,0 +1,15 @@
+"use strict";
+const myName=" Abdul Qadeer ";
+console.log(myName);
+console.log(myName.length);
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+console.log(myName.trim());
+console.log(myName.concat(" Khawaja"));
+console.log(myName.endsWith("ja"));
+console.log(myName.indexOf("e"));
+console.log(myName.substring(0,4));
+console.log(myName.slice(0,4));
+console.log(myName.replace("Abdul","A"));
+console.log(myName.charAt(1));
+console.log(myName.split(""));
