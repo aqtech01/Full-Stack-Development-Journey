f00bcfc7a5b50f8b2dcc4ac8fbd7f322dad7be1e
aqtech <abdulqadeer0120@gmail.com>
2024-08-30 12:05:49 +0100

    practise string and array object methods

---
+++ 03Basic/function.js
@@ -0,0 +1,16 @@
+"use strict";
+// function greetings() {
+//   console.log("Welcome");
+// }
+
+// greetings();
+
+// function addNumbers(num1, num2) {
+//   if (isNaN(num1) && isNaN(num2)) {
+//     return "Enter Numeric Value";
+//   } else {
+//     return num1 + num2;
+//   }
+// }
+// console.log(addNumbers("12", "12"));
+function addCartPrice(...params) {}
--- practise/strings.js
+++ practise/strings.js
@@ -1,23 +1,69 @@
-"use strict";
-const myName = " Abdul Qadeer ";
-// console.log(myName);
-// console.log(myName.length);
-// console.log(myName.toUpperCase());
-// console.log(myName.toLowerCase());
-// console.log(myName.trim());
-// console.log(myName.concat(" Khawaja"));
-// console.log(myName.endsWith("ja"));
-// console.log(myName.indexOf("e"));
-// console.log(myName.substring(0, 4));
-// console.log(myName.slice(0, 4));
-// console.log(myName.replace("Abdul", "A"));
-// console.log(myName.charAt(1));
-// let a = myName.split("");
-// for (let i = 0; i < a.length; i++) {
-//   const b = a[i];
-//   console.log(b);
-// }
-// // console.log(a);
+// "use strict";
+// const myName = " Abdul Qadeer ";
+// // console.log(myName);
+// // console.log(myName.length);
+// // console.log(myName.toUpperCase());
+// // console.log(myName.toLowerCase());
+// // console.log(myName.trim());
+// // console.log(myName.concat(" Khawaja"));
+// // console.log(myName.endsWith("ja"));
+// // console.log(myName.indexOf("e"));
+// // console.log(myName.substring(0, 4));
+// // console.log(myName.slice(0, 4));
+// // console.log(myName.replace("Abdul", "A"));
+// // console.log(myName.charAt(1));
+// // let a = myName.split("");
+// // for (let i = 0; i < a.length; i++) {
+// //   const b = a[i];
+// //   console.log(b);
+// // }
+// // // console.log(a);
 
-let c = Number(0.1 + 0.2);
-console.log(c.toFixed(1));
+// let c = Number(0.1 + 0.2);
+// console.log(c.toFixed(1));
+
+// const str = "This is Sparta";
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.slice(0, 4));
+// console.log(str.substring(0, 4));
+// console.log(str.includes("is", 3));
+
+// newValue = str.replace("is", "was");
+// console.log(newValue);
+
+// console.log(str.concat(" The World of Amaze"));
+
+// console.log(str.charAt(2));
+// console.log(str.charCodeAt(2));
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8];
+// let heros = ["Iron Man", "Thor"];
+// let newArray = [...num, ...heros];
+// console.log(newArray);
+
+// for (let i = 0; i < num.length; i++) {
+//   console.log(num[i]);
+
+// }
+// num.push(9);
+// num.push();
+// num.shift();
+// num.unshift(0);
+// num.slice(1, 2);
+// num.splice(1, 1);
+// console.log(num);
+
+// console.log(num.concat([1, 2, 3, 4, 5, 5]));
+// console.log(num.slice(1, 2));
+
+// const info = {
+//   name: "AQ",
+//   age: 23,
+//   location: "leeds",
+// };
+// console.log(info.age);
+// console.log(info["location"]);
+// console.log(Object.keys(info));
+// console.log(`${Object.keys(info)}\n ${Object.values(info)}`);
+// console.log(Object.entries(info));
