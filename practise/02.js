ed2024af266e08886865366f97c6a2584754b34d
Abdul Qadeer <abdulqadeer0120@gmail.com>
2024-09-08 11:09:20 +0000

    some Concepts

---
+++ practise/02.js
@@ -0,0 +1,25 @@
+"use strict ";
+const fruits = ["apple","mango","Banana","Grapse"];
+
+
+// console.log(fruits);
+// // console.log(fruits[0]);
+// for (const element of fruits) {
+//     console.log(element);
+    
+// }
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+// fruits.unshift("Strawberry");
+// console.log(fruits);
+
+
+
+// Destructing
+
+// fruits.push("Starwberry");
+// console.log(fruits);
+
+
