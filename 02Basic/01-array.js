511566941ada3205a161c01387d3a0a09504e3f8
aqtech <abdulqadeer0120@gmail.com>
2024-09-16 03:13:42 +0100

    new concepts

--- 02Basic/01-array.js
+++ 02Basic/01-array.js
@@ -1,20 +1,20 @@
 "use strict";
 // const myArray = [1, 2, 3, 4, 5, 6];
 
-const marvelHeros = [
-  "Capitan America",
-  "Iron Man",
-  "Spider Man",
-  "Natasha Romanov",
-  "Hawk Eye",
-];
-const dcHeros = [
-  "Super Man",
-  "Bat Man",
-  "Aqua Man",
-  "Wonder Women",
-  "The Flash",
-];
+// const marvelHeros = [
+//   "Capitan America",
+//   "Iron Man",
+//   "Spider Man",
+//   "Natasha Romanov",
+//   "Hawk Eye",
+// ];
+// const dcHeros = [
+//   "Super Man",
+//   "Bat Man",
+//   "Aqua Man",
+//   "Wonder Women",
+//   "The Flash",
+// ];
 // Array Methods
 // console.log(marvelHeros);
 // marvelHeros.push("Scarlet Witch");
@@ -49,3 +49,65 @@ const dcHeros = [
 // Flat Function in array
 
 // console.log(dcHeros.flat(2));
+// const marvelHeros = [
+//   "Capitan America",
+//   "Iron Man",
+//   "Spider Man",
+//   "Natasha Romanov",
+//   "Hawk Eye",
+// ];
+// const dcHeros = [
+//   "Super Man",
+//   "Bat Man",
+//   "Aqua Man",
+//   "Wonder Women",
+//   "The Flash",
+// ];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const multiplyByTwo = (numbers, index) => {
+//   console.log(`Index is ${index}`);
+//   console.log(`Multiple Number  is ${numbers * 2}`);
+// };
+// for (let index = 0; index < numbers.length; index++) {
+//   const element = numbers[index];
+//   multiplyByTwo(element, index);
+// }
+
+// const users = [{ name: "Abdul" }, { name: "Qadeer" }, { name: "Tanveer" }];
+// users.forEach((user) => {
+//   console.log(user.name);
+// });
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const numberFilter = numbers.sort((a, b) => a - b);
+// const numberFilter = numbers.map(
+//   (number, index) => `${number} Index: ${index}`
+// );
+// const numberFilter = numbers.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue
+// );
+// const numberFilter = numbers.filter((number) => number > 5);
+// console.log(numberFilter);
+
+// const products = [
+//   { ProductId: 1, productName: "p1", price: 100 },
+//   { ProductId: 2, productName: "p2", price: 500 },
+//   { ProductId: 3, productName: "p3", price: 4000 },
+//   { ProductId: 4, productName: "p4", price: 1100 },
+//   { ProductId: 5, productName: "p5", price: 10 },
+// ];
+// const checkPrice = products.every((pri) => pri.price < 10000);
+// console.log(checkPrice);
+// function isLength(string) {
+//   return string.productName === "p1";
+// }
+// const findProduct = products.find(isLength);
+// console.log(findProduct);
+// const SortedPrice = products.sort((a, b) => a.price - b.price);
+// console.log(SortedPrice);
+
+// const numbers = [1, 2, 3, 4, 5, , 6, 7, 8, 9, 10];
+// const checkEven = numbers.every((number) => number % 2 === 0);
+// console.log(checkEven);
---
+++ practise/03.js
@@ -0,0 +1,19 @@
+"use strict";
+// Strings
+
+// let abc =
+//   "https://github.com/aqtech01/Full-Stack-Development-Journey/tree/main/10-OOP";
+
+// console.log(abc.length);
+// console.log(abc.indexOf("Full"));
+// console.log(abc.charAt(5));
+// console.log(abc.toLowerCase());
+// console.log(abc.toUpperCase());
+// console.log(abc.lastIndexOf("OOP"));
+// console.log(abc.slice(8, abc.length - 6));
+// console.log(abc.replace("github", "aqtech"));
+// console.log(abc.split(""));
+
+const arr = [1, 2, 3, 4, 11, 13, 5, 6, 0, 7, 8, 19, 9];
+// console.log(arr.filter((age) => age > 5));
+console.log(arr.map(() => {}));
